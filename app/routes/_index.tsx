f18fcf8be1e0ad0e87d12dc2f61e08831e772d62
Halisia <halisia@MacBook-Pro-de-Halisia.local>
2024-09-01 19:34:35 +0200

    add sticky note

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,7 +24,7 @@ export default function Index() {
   return (
     <div>
       <button onClick={()=>setVisibleNote(true)} className=" bg-[#0c708c] absolute h-20 w-5 right-0 md:top-10 bottom-10 rounded-tl-lg rounded-bl-lg text-white text-3xl">|</button>
-      <div className={`${visibleNote ? "" : "hidden"} z-20 p-2 text-lg absolute w-7/12 bg-[#0c708c] bottom-0 md:top-0 right-0 h-4/12 md:h-1/5 rounded md:rounded-bl-xl text-white`}>
+      <div className={`${visibleNote ? "" : "hidden"} z-20 p-2 text-lg absolute w-7/12 bg-[#0c708c] bottom-0 md:top-0 right-0 h-4/12 md:h-fit rounded md:rounded-bl-xl text-white`}>
         <div className="relative">Welcome! 👋</div> <br /><button onClick={()=> setVisibleNote(false)} className="absolute top-4 right-3 w-5 "><img src="/images/commom/close.png"/></button>
         Thank you for visiting my portfolio. This site is currently a work in progress, and I’m excited to keep building it and give you a sneak peek into my professional journey. For now, you can explore my projects, and I look forward to sharing more updates soon. <br />Your feedback is always welcome!
       </div>
