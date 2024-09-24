073cef2313f3988d9da24740d9cb2d3b4d32b08e
Halisia <halifa.halisia@gmail.com>
2024-09-25 00:15:29 +0200

    last details

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -44,9 +44,9 @@ export const NavBar = () => {
                 </div>
                 <button className="flex lg:hidden px-5" onClick={sideMenuOpen ? closeSideMenu : openSideMenu}>
                     {sideMenuOpen ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="z-20">
-                            <path d="M9 18l6-6-6-6" />
-                        </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" className="z-20">
+                     <path d="M18.3 5.71a0.75 0.75 0 00-1.06 0L12 10.94 6.76 5.71a0.75 0.75 0 00-1.06 1.06L10.94 12l-5.24 5.24a0.75 0.75 0 101.06 1.06L12 13.06l5.24 5.24a0.75 0.75 0 001.06-1.06L13.06 12l5.24-5.24a0.75 0.75 0 000-1.06z"/>
+                   </svg>
                     ) : (
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30" className="z-20">
                             <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
