cccb5abc5490f3af40b993433c92f6c3e4a07c7c
Halisia <halifa.halisia@gmail.com>
2024-09-25 23:11:47 +0200

    remove picture

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,41 +1,36 @@
 export const Contact = () => {
     return (
-        <div id="contact" className="tracking-widest min-h-screen xl:h-screen w-screen flex-col flex justify-start pt-20 px-10 xl:px-20 text-gray-900">
+        <div id="contact" className="tracking-widest min-h-screen h-screen w-screen flex-col flex pt-20 px-10 xl:px-20 text-gray-900">
             <div className="text-3xl xl:text-[40px] font-serif w-full">
                 Work with me
             </div>
+            <div className="flex flex-col h-full justify-center space-y-10 text-lg xl:text-3xl">
+                <div className="flex flex-col space-y-2 w-3/4 lg:w-full">
+                    <div className="font-extralight text-xl">
+                        MOBILITY AVAILABILITY
+                    </div>
+                    <div className="font-extralight tracking-widest font-serif">
+                        Ile-de-France, France
 
-            <div className="flex-col flex xl:flex-row h-screen my-10 w-screen xl:w-3/4 justify-center xl:justify-between xl:mx-10 ">
-                <div className="self-center">
-                    <img src="images/home/presentation.png" className="w-3/4 xl:justify-end" />
+                    </div>
                 </div>
-                <div className="flex flex-col h-full justify-center space-y-10 text-lg xl:text-3xl">
-                    <div className="flex flex-col space-y-2 w-3/4 lg:w-full">
-                        <div className="font-extralight text-xl">
-                            MOBILITY AVAILABILITY
-                        </div>
-                        <div className="font-extralight tracking-widest font-serif">
-                            Ile-de-France, France
+                <div className="flex-col space-y-2">
+                    <div className="font-extralight mt-5 text-xl">
+                        PHONE
+                    </div>
+                    <div className="flex font-extralight tracking-widest font-serif">
+                        {" +336 77 86 08 24"}
+                    </div>
+                </div>
+                <div className="flex-col space-y-2">
+                    <div className="font-extralight mt-5 text-xl">
+                        EMAIL
+                    </div>
+                    <div className="flex font-extralight tracking-widest font-serif">
+                        {"halifa.halisia@gmail.com"}
+                    </div>
+                </div>
+            </div></div>
 
-                        </div>
-                    </div>
-                    <div className="flex-col space-y-2">
-                        <div className="font-extralight mt-5 text-xl">
-                            PHONE
-                        </div>
-                        <div className="flex font-extralight tracking-widest font-serif">
-                            {" +336 77 86 08 24"}
-                        </div>
-                    </div>
-                    <div className="flex-col space-y-2">
-                        <div className="font-extralight mt-5 text-xl">
-                            EMAIL
-                        </div>
-                        <div className="flex font-extralight tracking-widest font-serif">
-                            {"halifa.halisia@gmail.com"}
-                        </div>
-                    </div>
-                </div></div>
-        </div>
     )
 }
